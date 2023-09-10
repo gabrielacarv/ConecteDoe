30b1492012b02589a51e2322422842428c0910d4
Gabriel Alves Carvalho <gabriel_alvescarvalho@hotmail.com>
2023-09-10 17:47:03 -0300

    Adicionar arquivos de projeto.

---
+++ ConecteDoe.WebApp/Controllers/AutenticacaoController.cs
@@ -0,0 +1,24 @@
+﻿using ConecteDoe.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConecteDoe.WebApp.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Autenticar(UsuarioViewModel entidade)
+        {
+            if (entidade.Autenticado())
+                await Response.WriteAsync("Resultado Positivo");
+            else
+                await Response.WriteAsync("Resultado Negativo");
+            return null;
+        }
+    }
+}
---
+++ ConecteDoe.WebApp/Models/UsuarioViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ConecteDoe.WebApp.Models
+{
+    public class UsuarioViewModel
+    {
+        public string Usuario  { get; set; }
+        public string Senha { get; set; }
+        public bool ManterConectado { get; set; }
+
+        public bool Autenticado()
+        {
+            if(Usuario == "renato" && Senha == "123")
+                return true;
+            else
+                return false;
+        }
+    }
+}
